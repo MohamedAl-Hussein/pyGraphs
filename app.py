06c488136ee1968d0e114bd0f01d6beeffc67653
MohamedAl-Hussein <sn95racing.v6@gmail.com>
2021-06-12 11:32:50 -0400

    Create app entry point with sample data
    Draw sample of DirectedGraph and UndirectedGraph.

---
+++ app.py
@@ -0,0 +1,115 @@
+import dash
+import dash_cytoscape as cyto
+import dash_html_components as html
+
+from d_graph import DirectedGraph
+from ud_graph import UndirectedGraph
+
+
+def ud_graph_elements(graph: UndirectedGraph) -> []:
+    elements = list()
+    for vertex in graph.get_vertices():
+        config = {"data": {"id": vertex, "label": vertex}}
+        elements.append(config)
+
+    for src, dst in graph.get_edges():
+        config = {"data": {"source": src, "target": dst}}
+        elements.append(config)
+
+    return elements
+
+
+def d_graph_elements(graph: DirectedGraph) -> []:
+    elements = list()
+    for vertex in graph.get_vertices():
+        config = {"data": {"id": str(vertex), "label": str(vertex)}}
+        elements.append(config)
+
+    for src, dst, weight in graph.get_edges():
+        config = {"data": {"source": str(src), "target": str(dst), "weight": weight}}
+        elements.append(config)
+
+    return elements
+
+
+graph = UndirectedGraph(start_edges=['AE', 'AC', 'BE', 'CE', 'CD', 'CB', 'BD', 'ED', 'BH', 'QG', 'FG'])
+edges = [(0, 1, 19), (0, 6, 13), (0, 8, 14), (2, 4, 10), (3, 4, 14), (7, 10, 14), (11, 3, 13), (12, 1, 20), (12, 2, 7),
+         (12, 4, 18), (12, 10, 13), (12, 11, 4)]
+digraph = DirectedGraph(start_edges=edges)
+
+app = dash.Dash(__name__)
+
+app.layout = html.Div([
+    html.Div([
+        cyto.Cytoscape(
+            id="dGraph",
+            layout={"name": "cose"},
+            style={"width": "100%", "height": "400px"},
+            elements=d_graph_elements(digraph),
+            stylesheet=[
+                {
+                    "selector": "node",
+                    "style": {
+                        "content": "data(label)",
+                        "text-halign": "center",
+                        "text-valign": "center",
+                        "width": "30px",
+                        "height": "30px",
+                        "shape": "circle"
+                    }
+                },
+                {
+                    "selector": "edge",
+                    "style": {
+                        "label": "data(weight)",
+                        "text-rotation": "autorotate",
+                        "text-margin-y": "10px",
+                        "text-halign": "top",
+                        "text-valign": "top",
+                        "curve-style": "bezier",
+                        "target-arrow-color": "red",
+                        "target-arrow-shape": "triangle"
+                    }
+                },
+                {
+                    "selector": "[weight > 0]",
+                    "style": {
+                        "line-color": "blue"
+                    }
+                }
+            ]
+        )
+
+    ]),
+    html.Div([
+        cyto.Cytoscape(
+            id="udGraph",
+            layout={"name": "cose"},
+            style={"width": "100%", "height": "400px"},
+            elements=ud_graph_elements(graph),
+            stylesheet=[
+                {
+                    "selector": "node",
+                    "style": {
+                        "content": "data(label)",
+                        "text-halign": "center",
+                        "text-valign": "center",
+                        "width": "30px",
+                        "height": "30px",
+                        "shape": "circle"
+                    }
+                },
+                {
+                    "selector": "edge",
+                    "style": {
+                        "curve-style": "bezier",
+                    }
+                }
+            ]
+        )
+
+    ])
+])
+
+if __name__ == "__main__":
+    app.run_server(debug=True)
