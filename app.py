14c15fe8869882fac574ba9d0fc81a6c0ea14767
MohamedAl-Hussein <sn95racing.v6@gmail.com>
2021-06-12 22:23:35 -0400

    Create sidebar layout to navigate graph types

--- app.py
+++ app.py
@@ -1,9 +1,145 @@
 import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
 import dash_cytoscape as cyto
 import dash_html_components as html
+from dash.dependencies import Input, Output
 
 from d_graph import DirectedGraph
 from ud_graph import UndirectedGraph
+
+
+app = dash.Dash(__name__, external_stylesheets=[dbc.themes.BOOTSTRAP])
+
+# styling the sidebar
+SIDEBAR_STYLE = {
+    "position": "fixed",
+    "top": 0,
+    "left": 0,
+    "bottom": 0,
+    "width": "16rem",
+    "padding": "2rem 1rem",
+    "background-color": "#f8f9fa",
+}
+
+# padding for the page content
+CONTENT_STYLE = {
+    "margin-left": "18rem",
+    "margin-right": "2rem",
+    "padding": "2rem 1rem",
+}
+
+sidebar = html.Div(
+    [
+        html.H2("Sidebar", className="display-4"),
+        html.Hr(),
+        html.P(
+            "Data Structure / Algorithm", className="lead"
+        ),
+        dbc.Nav(
+            [
+                dbc.NavLink("Home", href="/", active="exact"),
+                dbc.NavLink("Directed Graph", href="/digraph", active="exact"),
+                dbc.NavLink("Undirected Graph", href="/graph", active="exact"),
+            ],
+            vertical=True,
+            pills=True,
+        ),
+    ],
+    style=SIDEBAR_STYLE,
+)
+
+content = html.Div(id="page-content", children=[], style=CONTENT_STYLE)
+
+app.layout = html.Div([
+    dcc.Location(id="url"),
+    sidebar,
+    content
+])
+
+
+@app.callback(Output("page-content", "children"), [Input("url", "pathname")])
+def render_page_content(pathname):
+    if pathname == "/":
+        return html.H1("Home Page")
+    elif pathname == "/digraph":
+        return html.Div([
+            cyto.Cytoscape(
+                id="dGraph",
+                layout={"name": "cose"},
+                style={"width": "100%", "height": "400px"},
+                elements=d_graph_elements(digraph),
+                stylesheet=[
+                    {
+                        "selector": "node",
+                        "style": {
+                            "content": "data(label)",
+                            "text-halign": "center",
+                            "text-valign": "center",
+                            "width": "30px",
+                            "height": "30px",
+                            "shape": "circle"
+                        }
+                    },
+                    {
+                        "selector": "edge",
+                        "style": {
+                            "label": "data(weight)",
+                            "text-rotation": "autorotate",
+                            "text-margin-y": "10px",
+                            "text-halign": "top",
+                            "text-valign": "top",
+                            "curve-style": "bezier",
+                            "target-arrow-color": "red",
+                            "target-arrow-shape": "triangle"
+                        }
+                    },
+                    {
+                        "selector": "[weight > 0]",
+                        "style": {
+                            "line-color": "blue"
+                        }
+                    }
+                ]
+            )
+        ])
+    elif pathname == "/graph":
+        return html.Div([
+            cyto.Cytoscape(
+                id="udGraph",
+                layout={"name": "cose"},
+                style={"width": "100%", "height": "400px"},
+                elements=ud_graph_elements(graph),
+                stylesheet=[
+                    {
+                        "selector": "node",
+                        "style": {
+                            "content": "data(label)",
+                            "text-halign": "center",
+                            "text-valign": "center",
+                            "width": "30px",
+                            "height": "30px",
+                            "shape": "circle"
+                        }
+                    },
+                    {
+                        "selector": "edge",
+                        "style": {
+                            "curve-style": "bezier",
+                        }
+                    }
+                ]
+            )
+        ])
+
+    # If the user tries to reach a different page, return a 404 message
+    return dbc.Jumbotron(
+        [
+            html.H1("404: Not found", className="text-danger"),
+            html.Hr(),
+            html.P(f"The pathname {pathname} was not recognised..."),
+        ]
+    )
 
 
 def ud_graph_elements(graph: UndirectedGraph) -> []:
@@ -37,79 +173,6 @@ edges = [(0, 1, 19), (0, 6, 13), (0, 8, 14), (2, 4, 10), (3, 4, 14), (7, 10, 14)
          (12, 4, 18), (12, 10, 13), (12, 11, 4)]
 digraph = DirectedGraph(start_edges=edges)
 
-app = dash.Dash(__name__)
-
-app.layout = html.Div([
-    html.Div([
-        cyto.Cytoscape(
-            id="dGraph",
-            layout={"name": "cose"},
-            style={"width": "100%", "height": "400px"},
-            elements=d_graph_elements(digraph),
-            stylesheet=[
-                {
-                    "selector": "node",
-                    "style": {
-                        "content": "data(label)",
-                        "text-halign": "center",
-                        "text-valign": "center",
-                        "width": "30px",
-                        "height": "30px",
-                        "shape": "circle"
-                    }
-                },
-                {
-                    "selector": "edge",
-                    "style": {
-                        "label": "data(weight)",
-                        "text-rotation": "autorotate",
-                        "text-margin-y": "10px",
-                        "text-halign": "top",
-                        "text-valign": "top",
-                        "curve-style": "bezier",
-                        "target-arrow-color": "red",
-                        "target-arrow-shape": "triangle"
-                    }
-                },
-                {
-                    "selector": "[weight > 0]",
-                    "style": {
-                        "line-color": "blue"
-                    }
-                }
-            ]
-        )
-
-    ]),
-    html.Div([
-        cyto.Cytoscape(
-            id="udGraph",
-            layout={"name": "cose"},
-            style={"width": "100%", "height": "400px"},
-            elements=ud_graph_elements(graph),
-            stylesheet=[
-                {
-                    "selector": "node",
-                    "style": {
-                        "content": "data(label)",
-                        "text-halign": "center",
-                        "text-valign": "center",
-                        "width": "30px",
-                        "height": "30px",
-                        "shape": "circle"
-                    }
-                },
-                {
-                    "selector": "edge",
-                    "style": {
-                        "curve-style": "bezier",
-                    }
-                }
-            ]
-        )
-
-    ])
-])
 
 if __name__ == "__main__":
     app.run_server(debug=True)
